db1ad70c9a8f80ec86109d3606d7804a4495294d
likhona <likhonapr@projectcodex.co>
2016-11-30 11:45:49 +0200

    latest

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 var request = require('request');
+var express = require('express');
+var bodyParser = require('body-parser');
+var handlebars = require("handlebars");
+var exphbs = require('express-handlebars');
+var app = express();
 
-request('http://www.google.com', function (error, response, body) {
+
+
+
+
+
+request('https://api.travis-ci.org/repos/Likhona7/53functions.json', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body) // Show the HTML for the Google homepage. 
+
+
   }
+
 })
