f6e47a4740a32c0806831b8e6f40bfbb7de7fabb
likhona <likhonapr@projectcodex.co>
2016-11-30 17:22:01 +0200

    latest

--- server.js
+++ server.js
@@ -10,12 +10,16 @@ var exphbs = require('express-handlebars');
 var app = express();
 
 
+var username = process.argv[2] || 'Onwa2014';
+var repoName = process.argv[3] || 'testTracker';
+
+var url = "https://api.travis-ci.org/repos/" + username + "/" + repoName + ".json";
+console.log(url);
+//running for which user and repo
+cron.schedule('*/5 * * * * *', function(){
 
 
-
-
-
-request('https://api.travis-ci.org/repos/Onwa2014/testTracker.json', function(error, response, body) {
+request(url, function(error, response, body) {
   if (!error && response.statusCode == 200) {
     var data = JSON.parse(body)
 
@@ -24,9 +28,7 @@ request('https://api.travis-ci.org/repos/Onwa2014/testTracker.json', function(er
     var time_started = data['last_build_started_at'];
 
     console.log(status);
-
     // console.log(time_started);
-
     // var now = moment(new Date()); //todays date// var end = moment("time_started"); // another date// var duration = moment.duration(now.diff(end));// var hours = duration.hours();// console.log(hours)
 
     var board = new five.Board();
@@ -36,11 +38,7 @@ request('https://api.travis-ci.org/repos/Onwa2014/testTracker.json', function(er
       var ledWhite = new five.Led(11);
       var ledOrange = new five.Led(10);
 
-
-
       // "blink" the led in 500ms on-off phase periods
-
-      cron.schedule('*/1 * * * *', function(){
 
         if(status == 1){
           ledRed.blink(100);
@@ -59,12 +57,11 @@ request('https://api.travis-ci.org/repos/Onwa2014/testTracker.json', function(er
           ledOrange.off();
         }
         console.log('running a task every one minutes');
-      });
-
 
 
 
     });
     // Show the HTML for the Google homepage.
   }
-})
+});
+});
