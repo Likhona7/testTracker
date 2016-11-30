f54994197d57e24d23ffea3cae17946573df676f
likhona <likhonapr@projectcodex.co>
2016-11-30 14:56:58 +0200

    led colors work

--- server.js
+++ server.js
@@ -10,21 +10,27 @@ var app = express();
 request('https://api.travis-ci.org/repos/Onwa2014/testTracker.json', function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var data = JSON.parse(body)
+
     // console.log(data)
-    console.log(data['last_build_status']);
-   // Show the HTML for the Google homepage.
+var status = data['last_build_status'];
+
+console.log(status);
+
+       // Show the HTML for the Google homepage.
   }
 })
-
-
-
 
 
 var board = new five.Board();
 board.on("ready", function() {
   var ledRed = new five.Led(13);
   var ledGreen = new five.Led(12);
+  var ledWhite = new five.Led(11);
+
   // "blink" the led in 500ms on-off phase periods
-  ledRed.blink(500);
-  ledGreen.blink(500);
+ledRed.on();
+ledGreen.on();
+ledWhite.on();
+
+  // ledRed.blink(500);
 });
