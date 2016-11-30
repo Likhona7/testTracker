574823af342b0e7757ed574557e6b15cd00cfdd6
likhona <likhonapr@projectcodex.co>
2016-11-30 14:28:16 +0200

    latest

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var five = require("johnny-five");
 var express = require('express');
 var bodyParser = require('body-parser');
 var handlebars = require("handlebars");
@@ -14,3 +15,16 @@ request('https://api.travis-ci.org/repos/Onwa2014/testTracker.json', function (e
    // Show the HTML for the Google homepage.
   }
 })
+
+
+
+
+
+var board = new five.Board();
+board.on("ready", function() {
+  var ledRed = new five.Led(13);
+  var ledGreen = new five.Led(12);
+  // "blink" the led in 500ms on-off phase periods
+  ledRed.blink(500);
+  ledGreen.blink(500);
+});
