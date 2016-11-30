681f6216be8b9bf57f7d4ef28535fbec7417a0b8
likhona <likhonapr@projectcodex.co>
2016-11-30 18:11:28 +0200

    latest

--- server.js
+++ server.js
@@ -17,38 +17,56 @@ var repoName = process.argv[3] || 'testTracker';
 var url = "https://api.travis-ci.org/repos/" + username + "/" + repoName + ".json";
 console.log(url);
 //running for which user and repo
-cron.schedule('*/5 * * * * *', function(){
+var board = new five.Board();
+board.on("ready", function() {
+
+  var ledRed = new five.Led(13);
+  var ledGreen = new five.Led(12);
+  var ledWhite = new five.Led(11);
+  var ledOrange = new five.Led(10);
 
 
-request(url, function(error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var data = JSON.parse(body)
 
-    // console.log(data)
-    var status = data['last_build_status'];
-    var time_started = data['last_build_started_at'];
+  cron.schedule('*/15 * * * * *', function() {
 
-    console.log(status);
-    // console.log(time_started);
-    // var now = moment(new Date()); //todays date// var end = moment("time_started"); // another date// var duration = moment.duration(now.diff(end));// var hours = duration.hours();// console.log(hours)
+    request(url, function(error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var data = JSON.parse(body)
 
-    var board = new five.Board();
-    board.on("ready", function() {
-      var ledRed = new five.Led(13);
-      var ledGreen = new five.Led(12);
-      var ledWhite = new five.Led(11);
-      var ledOrange = new five.Led(10);
+        // console.log(data)
+        var status = data['last_build_status'];
+        var time_started = data['last_build_started_at'];
 
-      // "blink" the led in 500ms on-off phase periods
+        console.log(status);
 
-        if(status == 1){
-          ledRed.blink(100);
+        // console.log(time_started);
+        // var now = moment(new Date()); //todays date// var end = moment("time_started"); // another date// var duration = moment.duration(now.diff(end));// var hours = duration.hours();// console.log(hours)
+        // "blink" the led in 500ms on-off phase periods
+
+        if (status == 1) {
+          ledRed.off();
+          ledGreen.off();
+          ledWhite.off();
+          ledOrange.off();
+
+          ledRed.on();
         }
+
         else if (status == 0) {
+          ledRed.off();
+          ledGreen.off();
+          ledWhite.off();
+          ledOrange.off();
+
           ledGreen.on();
         }
+        else if (status == null) {
 
-        else if(status == null){
+          ledRed.off();
+          ledGreen.off();
+          ledWhite.off();
+          ledOrange.off();
+
           ledWhite.on();
         }
         else {
@@ -57,12 +75,10 @@ request(url, function(error, response, body) {
           ledWhite.off();
           ledOrange.off();
         }
-        console.log('running a task every one minutes');
-
-
-
+        console.log('running a task every 15 minutes');
+      }
     });
+
     // Show the HTML for the Google homepage.
-  }
-});
+  });
 });
