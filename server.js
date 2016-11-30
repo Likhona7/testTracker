ce172db3dba62f9bc24eee68803d71f3d9cc96a1
likhona <likhonapr@projectcodex.co>
2016-11-30 16:23:07 +0200

    latest

--- server.js
+++ server.js
@@ -8,37 +8,50 @@ var exphbs = require('express-handlebars');
 var app = express();
 
 
-request('https://api.travis-ci.org/repos/Onwa2014/testTracker.json', function (error, response, body) {
+request('https://api.travis-ci.org/repos/Onwa2014/testTracker.json', function(error, response, body) {
   if (!error && response.statusCode == 200) {
     var data = JSON.parse(body)
 
     // console.log(data)
-var status = data['last_build_status'];
-var time_started = data['last_build_started_at'];
+    var status = data['last_build_status'];
+    var time_started = data['last_build_started_at'];
 
-console.log(status);
+    console.log(status);
 
-var now = moment(new Date()); //todays date
-var end = moment("time_started"); // another date
-var duration = moment.duration(now.diff(end));
-var hours = duration.hours();
-console.log(hours)
+    // console.log(time_started);
 
-       // Show the HTML for the Google homepage.
+    // var now = moment(new Date()); //todays date// var end = moment("time_started"); // another date// var duration = moment.duration(now.diff(end));// var hours = duration.hours();// console.log(hours)
+
+    var board = new five.Board();
+    board.on("ready", function() {
+      var ledRed = new five.Led(13);
+      var ledGreen = new five.Led(12);
+      var ledWhite = new five.Led(11);
+      var ledOrange = new five.Led(10);
+
+      // "blink" the led in 500ms on-off phase periods
+if(status == 1){
+  ledRed.blink(100);
+}
+else if (status == 0) {
+  ledGreen.on();
+
+}
+
+else if(status == null){
+  ledWhite.on();
+}
+else {
+  ledRed.off();
+  ledGreen.off();
+  ledWhite.off();
+  ledOrange.off();
+
+}
+
+
+      // ledRed.blink(500);
+    });
+    // Show the HTML for the Google homepage.
   }
 })
-
-
-var board = new five.Board();
-board.on("ready", function() {
-  var ledRed = new five.Led(13);
-  var ledGreen = new five.Led(12);
-  var ledWhite = new five.Led(11);
-
-  // "blink" the led in 500ms on-off phase periods
-  ledRed.on();
-ledGreen.on();
-ledWhite.on();
-
-  // ledRed.blink(500);
-});
