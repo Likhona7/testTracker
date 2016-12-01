14420a6bc320ceaeb24ff7e385170ad7722d78e1
likhona <likhonapr@projectcodex.co>
2016-12-01 09:51:11 +0200

    latest

--- server.js
+++ server.js
@@ -50,20 +50,23 @@ board.on("ready", function() {
             ledGreen.off();
             ledWhite.off();
             ledOrange.off();
+             piezo.stop();
           }
+//
           // console.log(time_started);
           // var now = moment(new Date()); //todays date// var end = moment("time_started"); // another date// var duration = moment.duration(now.diff(end));// var hours = duration.hours();// console.log(hours)
           // "blink" the led in 500ms on-off phase periods
 
         if (status == 1) {
+
+          piezo.play({
+             song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -",
+               beats: 1 / 4,
+               tempo: 5
+             });
+;
           switchOffLeds();
           ledRed.on();
-          // piezo.play({
-          //   song: "C D F D A - A A A A G G G G - - C D F D G - G G G G F F F F - -",
-          //     beats: 1 / 4,
-          //     tempo: 100
-          //   });
-          //
 
         } else if (status == 0) {
           switchOffLeds();
