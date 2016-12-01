24bb2553b0e71a22450f9a4c74b9154613609f10
Onwaba <onwab@projectcodex.co>
2016-12-01 12:33:08 +0200

    working commit time

--- server.js
+++ server.js
@@ -44,16 +44,26 @@ board.on("ready", function() {
 
 
         var dateTime = time_started.replace(/T/g, " ").replace(/Z/g, "");
-        var dateTime = dateTime.split(" ");
+        // var dateTime = dateTime.split(" ");
 
-var time = []
+        // console.log(time_started);
+        var now = moment(new Date());
+        //  todays date
+        var end = moment(dateTime); // another date//
+        console.log(end);
+        var duration = moment.duration(now.diff(end));//
+        var hours = duration.hours();//
+        console.log(hours + "hours")
 
-dateTime.forEach(function(array){
-var timeArray = [];
-timeArray.push(array)
-time.push(timeArray[1])
-})
-console.log(time);
+
+// var time = []
+
+// dateTime.forEach(function(array){
+// var timeArray = [];
+// timeArray.push(array)
+// time.push(timeArray[1])
+// })
+// console.log(time);
 
         var switchOffLeds = function() {
             ledRed.off();
@@ -63,13 +73,6 @@ console.log(time);
             // piezo.stop();
           }
           //
-        // console.log(time_started);
-        // var now = moment(new Date());
-        // //  todays date
-        // var end = moment("time_started"); // another date//
-        // var duration = moment.duration(now.diff(end));//
-        // var hours = duration.hours();//
-        // console.log(hours)
 
         // the led in 500 ms on - off phase periods
 
